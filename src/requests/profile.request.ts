547e1840ce60675fa4b9b87035ffd930f7695c6b
Dmitriy Karpenko <dmitriy.qua@gmail.com>
2022-03-30 22:13:37 +0300

    fix

--- src/requests/profile.request.ts
+++ src/requests/profile.request.ts
@@ -13,7 +13,7 @@ export class ProfileRequest {
     const queryParams = {
       q: 'memberIdentity',
       memberIdentity: publicIdentifier,
-      decorationId: 'com.linkedin.voyager.dash.deco.identity.profile.TopCardSupplementary-86',
+      decorationId: 'com.linkedin.voyager.dash.deco.identity.profile.FullProfileWithEntities-86',
     };
 
     return this.request.get<GetProfileResponse>('identity/dash/profiles', {
